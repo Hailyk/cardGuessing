8b2ca7675b86e6219609f6a4e6a4c60b23579ee2
NSagan271 <naomi.sagan@gmail.com>
2018-05-25 14:50:14 -0700

    Add capability to serve webpage

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const socket = require('socket.io');
 const data = require('./data.js');
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
@@ -9,9 +11,9 @@ const server = http.createServer(app);
 
 const io = socket.listen(server);
 
-setUpData();
+app.use(express.static("../client"));
 
-io.on('connection', function(socket){
+io.on('connection', function(socket, callback){
   socket.on('new user', function(callback){
     const indentifier = data.newUser();
     if (data.addSocketId(identifier, socket.id)) callback(identifier);
@@ -21,13 +23,13 @@ io.on('connection', function(socket){
     if (data.addSocketId(identifier, socket.id)) callback(identifier);
     else callback (null);
   });
-  socket.on('credit', function(balance){
-    updateBalance(balance);
+  socket.on('credit', function(callback){
+    callback(data.getBalance());
   });
-  socket.on('choice', function(guess, betAmount){
+  socket.on('choice', function(guess, betAmount, callback){
     const winner = Math.floor(Math.random()*4);
     if (guess == winner){
-      updateBalance(socket.id, data.getBalance(socket.id)+betAmount*2);
+      data.updateBalance(socket.id, data.getBalance(socket.id)+betAmount*2);
       data.incrementWins(socket.id);
     }
     else data.updateBalance(socket.id, data.getBalance(socket.id)-betAmount);
@@ -48,3 +50,21 @@ function setUpData(){
     }
   });
 }
+
+app.get("/", function(req, res){
+  fs.readFile(path.resolve(path.join(__dirname,'../client/index.html')),function(err, html){
+        if(err) res.end(err);
+        else{
+            res.writeHeader(200, {"Content-Type": "text/html"});
+            res.write(html);
+            res.end();
+        }
+    });
+});
+
+var host = process.env.HOST || 'localhost';
+var port = process.env.PORT || '3000';
+server.listen(port, host, function () {
+  console.log("Hosting webpage");
+  setUpData();
+});
